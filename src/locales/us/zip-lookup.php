ee5daa6d5781bcb65a47b40a55b07f22bccd08d0
Chris Wagner <christopher.t.wagner@gmail.com>
2012-02-13 16:00:41 -0300

    src/locales/us/zip-lookup.php: Rewrite 'lookupZipCode' to work with the updated USPS website/form.

--- src/locales/us/zip-lookup.php
+++ src/locales/us/zip-lookup.php
@@ -2,30 +2,37 @@
 
 namespace MyPHPLibs\Locales\US;
 
-require_once dirname(dirname(dirname(__FILE__))) . '/http-client.php';
+require_once dirname(dirname(dirname(__FILE__))) . '/web-browser.php';
 require_once dirname(dirname(dirname(__FILE__))) . '/us-address.php';
 
-use \HttpClient, \ZipCode;
+use \MyPHPLibs\WebBrowsing\WebBrowser, \ZipCode;
 
-function lookupZipCode($addr, HttpClient $client) {
-  $vars = array('visited' => '1', 'pagenumber' => '0', 'firmname' => '',
-                'address1' => $addr['street1'], 'address2' => $addr['street2'],
-                'city' => $addr['city'], 'state' => $addr['state'], 'urbanization' => '',
-                'zip5' => $addr['zip5']);
-  $response = $client->post('http://zip4.usps.com/zip4/zcl_0_results.jsp', $vars);
-  $matches = null;
-  preg_match_all('/[0-9]{5}-[0-9]{4}/', $response->content, $matches);
-  if (count($matches[0]) >= 2) {
-    $parts = explode('-', $matches[0][1]);
-    return new ZipLookupResult(new ZipCode($parts[0], $parts[1]));
-    if (stristr($response->content, 'returned more than one result')) {
+function lookupZipCode($addr, WebBrowser $client) {
+  $vars = array('mode' => 0,
+                'tAddress' => $addr['street1'], 'tApt' => $addr['street2'],
+                'tCity' => $addr['city'], 'sState' => $addr['state'],
+                'zip' => $addr['zip5']);
+  $client->follow_redirect = true;
+  $response = $client->post('https://tools.usps.com/go/ZipLookupAction.action', $vars);
+  $errNode = current($client->findMatchingNodes("//p[@id='nonDeliveryMsg']"));
+  $z5node = current($client->findMatchingNodes("//div[@id='result-list']//span[@class='zip']"));
+  if ($errNode) {
+    $err = $errNode->textContent;
+    if (stristr($err, 'We were unable to process your request')) {
+      throw new UnableToProcessRequest;
+    } else if (stristr($err, 'not recognized by the US Postal Service')) {
+      throw new AddressNotRecognized;
+    } else {
+      throw new ZipCodeLookupError("Did not recognize following error message given by " .
+        "USPS website: " . $err);
+    }
+  } else if ($z5node) {
+    $z4node = current($client->findMatchingNodes("//div[@id='result-list']//span[@class='zip4']"));
+    return new ZipLookupResult(new ZipCode($z5node->textContent, $z4node->textContent));
+    if (stristr($response->content, 'Several addresses matched the information you provided')) {
       $result->multipleResults = true;
     }
     return $result;
-  } else if (stristr($response->content, 'We were unable to process your request')) {
-    throw new UnableToProcessRequest;
-  } else if (stristr($response->content, 'not recognized by the US Postal Service')) {
-    throw new AddressNotRecognized;
   } else {
     throw new ZipCodeLookupError("Could not find ZIP Code or error message in data returned " .
       "from USPS website (zip4.usps.com)");
