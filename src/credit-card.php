0058fe9bb68d460957ebb1aaf05a473502a021af
Chris Wagner <christopher.t.wagner@gmail.com>
2012-06-15 21:31:18 -0300

    src/credit-card.php: Place this file under appropriate namespace, rename the one function it has in light of that, and update test-case accordingly.

--- src/credit-card.php
+++ src/credit-card.php
@@ -1,11 +1,13 @@
 <?php
 
-function maskCreditCardNumber($untrimmed) {
+namespace MyPHPLibs\CreditCard;
+
+function maskCardNumber($untrimmed) {
   $cardnumber = str_replace(' ', '', $untrimmed);
   $matches = array();
   preg_match('/^([0-9]{4})([-.0-9]*)([0-9]{4})$/', $cardnumber, $matches);
   if (empty($matches)) { 
-    throw new InvalidArgumentException("Invalid credit card number given: '$untrimmed'");
+    throw new \InvalidArgumentException("Invalid credit card number given: '$untrimmed'");
   }
   $firstFour = $matches[1];
   $masked = preg_replace('/[0-9]/', '*', $matches[2]);
--- test/credit-card.php
+++ test/credit-card.php
@@ -1,20 +1,21 @@
 <?php
 
 require_once 'credit-card.php';
+use \MyPHPLibs\CreditCard;
 
 function testMaskingCreditCardNumber() {
-  assertEqual('1234-****-****-4321', maskCreditCardNumber('1234-5678-8765-4321'));
-  assertEqual('1111********2222', maskCreditCardNumber('1111555599992222'));
-  assertEqual('1111********2222', maskCreditCardNumber('1111 5555 9999 2222'));
-  assertEqual('1234*4321', maskCreditCardNumber('123454321'));
+  assertEqual('1234-****-****-4321', CreditCard\maskCardNumber('1234-5678-8765-4321'));
+  assertEqual('1111********2222', CreditCard\maskCardNumber('1111555599992222'));
+  assertEqual('1111********2222', CreditCard\maskCardNumber('1111 5555 9999 2222'));
+  assertEqual('1234*4321', CreditCard\maskCardNumber('123454321'));
 }
 
 function testMaskCreditCardNumberGracefullyHandlesInvalidCardNumber() {
   $badValues = array('4301', '3422$1798z4844#7732', '123412');
   foreach ($badValues as $v) {
     try {
-      maskCreditCardNumber($v);
-      fail("Expected 'maskCreditCardNumber' to raise exception for value '$v'");
+      CreditCard\maskCardNumber($v);
+      fail("Expected 'maskCardNumber' to raise exception for value '$v'");
     } catch (InvalidArgumentException $e) {
       // That's expected...
     }
