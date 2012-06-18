83a667dd3c5a297a3c43a28f168fd7626a80685e
Chris Wagner <christopher.t.wagner@gmail.com>
2012-06-18 18:47:15 -0300

    src/csv.php: Add function 'databaseResultToCsv', intended for taking an associative-array result returned directly from a database API (PDO, specifically) and converting it to CSV content.

--- src/csv.php
+++ src/csv.php
@@ -11,3 +11,17 @@ function arrayToCsv($rows, $columnsToShow) {
   }
   return $output;
 }
+
+# This function is intended for taking an associative-array result returned directly from a
+# database API (PDO, specifically) and converting it to CSV content.  The thing to note, is,
+# any array keys that are integers will be ignored...  For example, an array like
+#   array( array('col1' => 'a', 0 => 'a', 'col2' => 'b', 1 => 'b'),
+#          array('col1' => 'c', 0 => 'c', 'col2' => 'd', 1 => 'd'))
+# would yield the following CSV content: "col1,col2\na,b\nc,d"
+function databaseResultToCsv($dbRows) {
+  $columnsToShow = array();
+  foreach ($dbRows[0] as $col => $_) {
+    if (!is_int($col)) $columnsToShow []= $col;
+  }
+  return arrayToCsv($dbRows, $columnsToShow);
+}
