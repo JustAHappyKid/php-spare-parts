b53060a7bd09f2423c1a85564c5924c9fdfcc7c1
Chris Wagner <christopher.t.wagner@gmail.com>
2013-04-27 18:02:18 -0300

    src/web-client/http-simple.php: Re-implement 'get' using HttpClient, as opposed to curl.

--- src/web-client/http-simple.php
+++ src/web-client/http-simple.php
@@ -2,13 +2,25 @@
 
 namespace MyPHPLibs\WebClient\HttpSimple;
 
+require_once dirname(__FILE__) . '/http-client.php';
+use \MyPHPLibs\WebClient\HttpClient;
+
+function get($url) {
+  $c = new HttpClient;
+  $r = $c->get($url);
+  return $r->content;
+}
+
+/*
 function get($url) {
   $ch = curl_init();
-  $timeout = 5;
+  $timeout = 30;
   curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
   curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
   $data = curl_exec($ch);
+  echo "data: $data\n";
   curl_close($ch);
   return $data;
 }
+*/
