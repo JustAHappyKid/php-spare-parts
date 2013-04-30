a273e686cadcaae4fe5001816e0089bd337ba27b
Chris Wagner <christopher.t.wagner@gmail.com>
2013-04-30 15:27:49 -0400

    src/email.php: Rename core function 'sendTextEmail' to be explicit that its intended for plain-text email (but also keep in place function by name 'sendEmail' for backwards-compatibility, for the moment); add email-address validation to $from and $to parameters; by default include 'From', 'Reply-To', 'Return-Path', and 'Content-Type' headers; and, specify '-f' switch/parameter for passing to the underlying sendmail (or similar) program, to be sure the envelope sender is also set.

--- src/email.php
+++ src/email.php
@@ -1,6 +1,21 @@
 <?php
 
+require_once dirname(__FILE__) . '/validation.php'; # isValidEmailAddr
+
+use \InvalidArgumentException;
+
 function sendEmail($from, $to, $subject, $message, Array $headers = array()) {
-  $allHeaders = array_merge(array("From: $from"), $headers);
-  mail($to, $subject, $message, implode("\r\n", $allHeaders));
+  sendTextEmail($from, $to, $subject, $message, $headers);
+}
+
+function sendTextEmail($from, $to, $subject, $message, Array $headers = array()) {
+  if (!isValidEmailAddr($from, $allowExtendedFormat = true)) {
+    throw new InvalidArgumentException("Invalid email address for \$from parameter: $from");
+  } else if (!isValidEmailAddr($to, $allowExtendedFormat = true)) {
+    throw new InvalidArgumentException("Invalid email address for \$to parameter: $to");
+  }
+  $defaultHeaders = array("From: $from", "Reply-To: $from", "Return-Path: $from",
+                          "Content-Type: text/plain");
+  $allHeaders = array_merge($defaultHeaders, $headers);
+  mail($to, $subject, $message, implode("\r\n", $allHeaders), "-f \"$from\"");
 }
