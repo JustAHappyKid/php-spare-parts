ecac89333103b4d09e6d686573933bd0927f1e8e
Chris Wagner <christopher.t.wagner@gmail.com>
2013-05-02 14:04:52 -0400

    src/array.php: Add implementation for 'takeWhile', a classic functional-programming tool/function; minor addition to comment for 'head' function.

--- src/array.php
+++ src/array.php
@@ -2,13 +2,16 @@
 
 namespace MyPHPLibs\ArrayLib;
 
+use \Closure;
+
 /**
  * This method may at first appear to be a useless wrapper for 'reset', but it serves two
  * little purpsoses: (1) it can be used on non-variables, and (2) it adds readability (at
  * least for those accustomed to functional programming languages).
  * 
  * Re (1): 'reset' cannot be used in a case such as this: reset(functionReturningArray())
- * Such would yield an error, as 'functionReturningArray()' is not a variable.
+ * Such would yield an error, as 'functionReturningArray()' is not a variable. The following
+ * will work, however: head(functionReturningArray())
  */
 function head(Array $a) {
   return reset($a);
@@ -18,4 +21,16 @@ function flatten(Array $origArray) {
   $flatArray = array();
   foreach ($origArray as $a) $flatArray = array_merge($flatArray, $a);
   return $flatArray;
+}
+
+function takeWhile(Closure $qualify, Array $a) {
+  $result = array();
+  foreach ($a as $item) {
+    if ($qualify($item)) {
+      $result []= $item;
+    } else {
+      break;
+    }
+  }
+  return $result;
 }
