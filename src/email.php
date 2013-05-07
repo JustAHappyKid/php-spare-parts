e4b32d4472f4c6faec7e140efe4c0991d1726718
Chris Wagner <christopher.t.wagner@gmail.com>
2013-05-06 20:12:34 -0400

    src/email.php: Allow multiple, comma-separated email addresses to be specified in $to parameter.

--- src/email.php
+++ src/email.php
@@ -11,8 +11,12 @@ function sendEmail($from, $to, $subject, $message, Array $headers = array()) {
 function sendTextEmail($from, $to, $subject, $message, Array $headers = array()) {
   if (!V\isValidEmailAddr($from, $allowExtendedFormat = true)) {
     throw new InvalidArgumentException("Invalid email address for \$from parameter: $from");
-  } else if (!V\isValidEmailAddr($to, $allowExtendedFormat = true)) {
-    throw new InvalidArgumentException("Invalid email address for \$to parameter: $to");
+  }
+  $allRecips = array_map('trim', explode(',', $to));
+  foreach ($allRecips as $r) {
+    if (!V\isValidEmailAddr($r, $allowExtendedFormat = true)) {
+      throw new InvalidArgumentException("Invalid email address in \$to parameter: $r");
+    }
   }
   $defaultHeaders = array("From: $from", "Reply-To: $from", "Return-Path: $from",
                           "Content-Type: text/plain");
