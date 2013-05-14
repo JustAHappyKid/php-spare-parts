2ba84dd6ebabc01993cc97a83a4e241221698a3e
Chris Wagner <christopher.t.wagner@gmail.com>
2013-05-14 11:18:00 -0400

    test/string.php: Add a few test assertions for new 'contains' function.

--- test/string.php
+++ test/string.php
@@ -2,6 +2,16 @@
 
 require_once 'string.php';
 
+function testContains() {
+  assertTrue(contains('abc', 'abc'));
+  assertTrue(contains('abc123', 'abc'));
+  assertTrue(contains('456abc', 'abc'));
+  assertTrue(contains('456abc', 'a'));
+  assertFalse(contains('456abc', 'd'));
+  assertFalse(contains('456abc', 'bcd'));
+  assertFalse(contains('456abc', 'bca'));
+}
+
 function testWithoutSuffix() {
   assertEqual('index', withoutSuffix('index.php', '.php'));
   assertEqual('index.php', withoutSuffix('index.php', '.txt'));
