8beda0fe998865548dcd9661565f291029a7a9ee
Chris Wagner <christopher.t.wagner@gmail.com>
2013-05-30 18:48:10 -0400

    Add src/database/paranoid.php, the very beginnings of a "paranoid" layer for database access, to limit the type of content that can be saved to the database (so far, explicitly limiting angle brackets to avoid malicious HTML code -- i.e., <script> tags) from making its way into the DB.

---
+++ src/database/paranoid.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace SpareParts\Database\Paranoid;
+
+require_once dirname(dirname(__FILE__)) . '/database.php';
+use \SpareParts\Database as DB, \Exception;
+
+# TODO: Extend this -- allow custom "policies", etc.
+
+function insertOne($table, Array $values, $returnId = false, $policy = null) {
+  foreach ($values as $column => $value) {
+    if (contains($value,  '<') || contains($value,  '>') ||
+        contains($column, '<') || contains($column, '>')) {
+      throw new Exception('No angle brackets allowed!');;
+    }
+  }
+  return DB\insertOne($table, $values, $returnId);
+}
+
+/*
+function updateByID($table, $id, Array $valuesToUpdate) {
+  TODO: Implement this function!
+  return DB\updateByID($table, $id, $valuesToUpdate);
+}
+*/
