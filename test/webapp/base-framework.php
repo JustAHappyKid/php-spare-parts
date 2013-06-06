84917dcd99e4232b1f94a0889632411716ae4960
Chris Wagner <christopher.t.wagner@gmail.com>
2013-06-06 00:36:32 -0400

    test/webapp/base-framework.php: Reimplement BaseFrameworkTests using \SpareParts\SelfTesting\WebappTestHarness.

--- test/webapp/base-framework.php
+++ test/webapp/base-framework.php
@@ -1,20 +1,25 @@
 <?php
 
-require_once 'webapp/base-framework.php';
+require_once dirname(__FILE__) . '/harness.php';
 
-use \SpareParts\Test\TestHarness, \SpareParts\Webapp;
+use \SpareParts\SelfTesting\WebappTestHarness, \SpareParts\Webapp,
+  \SpareParts\Test\HttpNotFound, \SpareParts\Test\HttpRedirect;
 
-class BaseFrameworkTests extends TestHarness {
+class BaseFrameworkTests extends WebappTestHarness {
 
   function testPathComponentsAreDecoded() {
-    $response = $this->mockRequest('GET', "/hitit/sumthin'%20to%20dec%40de");
+    $response = $this->get("/hitit/sumthin'%20to%20dec%40de");
     assertTrue(strstr($response->content, "sumthin' to dec@de") != false,
       'Decoded value should be in following output: ' . $response->content);
   }
 
   function testRedirecting() {
-    $response = $this->mockRequest('GET', '/do-redirect');
-    assertTrue($response->statusCode >= 300 && $response->statusCode < 400);
+    try {
+      $response = $this->get('/do-redirect');
+      assertTrue($response->statusCode >= 300 && $response->statusCode < 400);
+    } catch (HttpRedirect $e) {
+      assertTrue($e->statusCode >= 300 && $e->statusCode < 400);
+    }
   }
 
   # This use-case should not occur in production (unless something's misconfigured?), but
@@ -23,7 +28,7 @@ class BaseFrameworkTests extends TestHarness {
     $_SERVER['REQUEST_METHOD'] = 'GET';
     $_SERVER['REQUEST_URI'] = "/do-redirect";
     unset($_SERVER['HTTP_HOST']);
-    $fc = new FrontControllerForTesting(dirname(__FILE__));
+    $fc = new \SpareParts\SelfTesting\FrontControllerForTesting(dirname(__FILE__));
     try {
       $response = $fc->handleRequest();
       fail('Expected exception to be raised since HTTP_HOST is not set');
@@ -31,33 +36,11 @@ class BaseFrameworkTests extends TestHarness {
   }
 
   function testCaseWhereDirectoryIsMirroredByFileOfSameName() {
-    $r = $this->mockRequest('GET', '/dir1/dir2/');
+    # In the actions directory, both a directory named 'dir1' and a file name 'dir1.php' exist...
+    # In the request here, the file (dir1.php) should be ignored, and this should map to
+    # 'dir1/dir2/index.php'.
+    $r = $this->get('/dir1/dir2/');
     assertTrue(strstr($r->content, 'Welcome to the index of dir2') != false,
       'Expected to reach the action in file dir1/dir2/index.php');
-  }
-
-  private function mockRequest($method, $uri) {
-    $_SERVER['HTTP_HOST'] = 'test.net';
-    $_SERVER['REQUEST_METHOD'] = $method;
-    $_SERVER['REQUEST_URI'] = $uri;
-    $fc = new FrontControllerForTesting(dirname(__FILE__));
-    return $fc->handleRequest();
-  }
-}
-
-class FrontControllerForTesting extends Webapp\FrontController {
-  protected function info($msg) { }
-  protected function notice($msg) { }
-  protected function warn($msg) { }
-  protected function sessionLifetimeInSeconds() { return 60 * 60 * 3; /* 3 hours */ }
-  protected function checkAccessPrivileges($cmd, $user) { return true; }
-  
-  # XXX: Should we not have a base-line implementation of this in the base FrontController??
-  protected function renderAndOutputPage($page) {
-    $response = new Webapp\HttpResponse;
-    $response->statusCode = 200;
-    $response->contentType = $page->contentType;
-    $response->content = $page->body;
-    return $response;
   }
 }
