450f770a8a816b57962f8bc4404a0bc6d89a9cdc
Chris Wagner <christopher.t.wagner@gmail.com>
2013-07-15 21:44:14 -0400

    src/csv.php: Place functions of this file under appropriate namespace, \SpareParts\CSV, and rename functions to more appropriate (?) names.

--- src/csv.php
+++ src/csv.php
@@ -1,7 +1,11 @@
 <?php
 
-# Convert an associated array to a string in "comma-separated values" format.
-function arrayToCsv($rows, $columnsToShow) {
+namespace SpareParts\CSV;
+
+/**
+ * Convert an associated array to a string in "Comma-Separated Values" format.
+ */
+function fromAssociativeArray($rows, $columnsToShow) {
   $output = implode(',', $columnsToShow);
   foreach ($rows as $r) {
     $output .= "\n";
@@ -12,16 +16,18 @@ function arrayToCsv($rows, $columnsToShow) {
   return $output;
 }
 
-# This function is intended for taking an associative-array result returned directly from a
-# database API (PDO, specifically) and converting it to CSV content.  The thing to note, is,
-# any array keys that are integers will be ignored...  For example, an array like
-#   array( array('col1' => 'a', 0 => 'a', 'col2' => 'b', 1 => 'b'),
-#          array('col1' => 'c', 0 => 'c', 'col2' => 'd', 1 => 'd'))
-# would yield the following CSV content: "col1,col2\na,b\nc,d"
-function databaseResultToCsv($dbRows) {
+/**
+ * This function is intended for taking an associative-array result returned directly from a
+ * database API (PDO, specifically) and converting it to CSV content.  The thing to note, is,
+ * any array keys that are integers will be ignored...  For example, an array like
+ *   array( array('col1' => 'a', 0 => 'a', 'col2' => 'b', 1 => 'b'),
+ *          array('col1' => 'c', 0 => 'c', 'col2' => 'd', 1 => 'd') )
+ * would yield the following CSV content: "col1,col2\na,b\nc,d"
+ */
+function fromDatabaseResult($dbRows) {
   $columnsToShow = array();
   foreach ($dbRows[0] as $col => $_) {
     if (!is_int($col)) $columnsToShow []= $col;
   }
-  return arrayToCsv($dbRows, $columnsToShow);
+  return fromAssociativeArray($dbRows, $columnsToShow);
 }
