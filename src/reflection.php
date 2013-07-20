433898ba7a06107810a08748ef10af930c6075ac
Chris Wagner <christopher.t.wagner@gmail.com>
2013-07-20 18:49:37 -0400

    src/reflection.php: Add methods 'implementsInterface' (which returns true if the given $cls implements the interface $iface), 'getImplementations' (which returns all [loaded] classes that implement the given interface), and 'getClassesDefinedInFile' (which, somewhat primitively, attempts to return the names of all classes defined in the specified file).

--- src/reflection.php
+++ src/reflection.php
@@ -17,6 +17,17 @@ function getSubclasses($parentClassName) {
   return $classes;
 }
 
+function implementsInterface($cls, $iface) {
+  return in_array($iface, class_implements($cls));
+}
+
+function getImplementations($interfaceName) {
+  return array_filter(get_declared_classes(),
+    function($className) use($interfaceName) {
+      return implementsInterface($className, $interfaceName);
+  });
+}
+
 function isAbstractClass($className) {
   $class = new ReflectionClass($className);
   return $class->isAbstract();
@@ -29,4 +40,17 @@ function getNamesOfPublicMethods($object) {
     $names []= $m->getName();
   }
   return $names;
+}
+
+function getClassesDefinedInFile($path) {
+  $classes = array();
+  $contents = file_get_contents($path);
+  $tokens = token_get_all($contents);
+  for ($i = 0; $i < count($tokens); ++$i) {
+    if (is_array($tokens[$i]) && $tokens[$i][0] === T_CLASS) {
+      $i += 2;
+      $classes[] = $tokens[$i][1];
+    }
+  }
+  return $classes;
 }
