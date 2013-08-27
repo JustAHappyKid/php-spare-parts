a11a865ed07f8b08bd658e7fd0a4304a344667b0
Chris Wagner <christopher.t.wagner@gmail.com>
2013-08-26 22:24:34 -0400

    src/template/token.php: Add function 'isOfType'.

--- src/template/token.php
+++ src/template/token.php
@@ -5,3 +5,7 @@ namespace SpareParts\Template\Token;
 function render($t) {
   return is_array($t) ? $t[1] : $t;
 }
+
+function isOfType($token, $type) {
+  return is_array($token) && $token[0] == $type;
+}
