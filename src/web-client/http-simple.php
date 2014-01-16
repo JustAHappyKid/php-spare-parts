833d290265c33b6ba7b5359ffcb9767aa108693e
Chris Wagner <christopher.t.wagner@gmail.com>
2014-01-16 11:30:56 -0500

    src/web-client/http-simple.php: Remove commented out (curl-based) implementation of 'get'.

--- src/web-client/http-simple.php
+++ src/web-client/http-simple.php
@@ -10,17 +10,3 @@ function get($url) {
   $r = $c->get($url);
   return $r->content;
 }
-
-/*
-function get($url) {
-  $ch = curl_init();
-  $timeout = 30;
-  curl_setopt($ch, CURLOPT_URL, $url);
-  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-  curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-  $data = curl_exec($ch);
-  echo "data: $data\n";
-  curl_close($ch);
-  return $data;
-}
-*/
