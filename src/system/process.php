3ae424f03995087e9fb319e41f6472005565327a
Chris Wagner <christopher.t.wagner@gmail.com>
2014-03-25 19:41:26 -0400

    src/system/process.php: Add comment for function 'runExclusiveProcess'.

--- src/system/process.php
+++ src/system/process.php
@@ -2,6 +2,12 @@
 
 namespace SpareParts\System\Process;
 
+/**
+ * Run $func as an "exclusive process", one that should only have one active instance
+ * at any given time. This is done by using a lock/PID file; if an existing file is found
+ * at the location specified by $pidFile, a notification will be emitted (to stdout) and
+ * the current PHP process will exit.
+ */
 function runExclusiveProcess($pidFile, $func) {
   global $__SpareParts_System_Process_PIDFiles;
   $__SpareParts_System_Process_PIDFiles = array();
