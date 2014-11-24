d923218bcb57eb26f0219c3dd2f8d8a20f2bcf51
Chris Wagner <christopher.t.wagner@gmail.com>
2014-11-24 13:59:03 -0500

    src/: Add dates.php as a temporary and deprecated way of accessing date-time.php.

---
+++ src/dates.php
@@ -0,0 +1,6 @@
+<?php
+
+# DEPRECATED: This file is deprecated in favor of date-time.php. Please include/require that
+# file directly; this one (dates.php) will eventually be removed.
+
+require_once dirname(__FILE__) . '/date-time.php';
