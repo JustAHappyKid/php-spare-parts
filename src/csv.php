ac0a9cb582a180cc883cdcbe2aa09e4b1d960dc8
Chris Wagner <christopher.t.wagner@gmail.com>
2015-07-13 13:36:15 -0400

    src/csv.php: Refactor 'fromAssociativeArray'; improve comment for 'fromDatabaseResult' a tad.

--- src/csv.php
+++ src/csv.php
@@ -6,12 +6,18 @@ namespace SpareParts\CSV;
  * Convert an associative array to a string in Comma-Separated Values (CSV) format.
  */
 function fromAssociativeArray($rows, $columnsToShow) {
-  $output = implode(',', $columnsToShow);
+
+  $makeRow = function($values) {
+    $escaped = array_map(function($v) { return str_replace('"', '""', $v); }, $values);
+    return '"' . implode('","', $escaped) . '"';
+  };
+
+  $output = $makeRow($columnsToShow);
   foreach ($rows as $r) {
     $output .= "\n";
     $values = array();
-    foreach ($columnsToShow as $c) $values[$c] = str_replace('"', '""', $r[$c]);
-    $output .= '"' . implode('","', $values) . '"';
+    foreach ($columnsToShow as $c) $values[$c] = $r[$c];
+    $output .= $makeRow($values);
   }
   return $output;
 }
@@ -20,9 +26,15 @@ function fromAssociativeArray($rows, $columnsToShow) {
  * This function is intended for taking an associative-array result returned directly from a
  * database API (PDO, specifically) and converting it to CSV content.  The thing to note, is,
  * any array keys that are integers will be ignored...  For example, an array like
+ *
  *   array( array('col1' => 'a', 0 => 'a', 'col2' => 'b', 1 => 'b'),
  *          array('col1' => 'c', 0 => 'c', 'col2' => 'd', 1 => 'd') )
- * would yield the following CSV content: "col1,col2\na,b\nc,d"
+ *
+ * would yield CSV content equivalent to the following:
+ *
+ *   col1,col2
+ *   a,b
+ *   c,d
  */
 function fromDatabaseResult(Array $dbRows) {
   if (count($dbRows) == 0)
