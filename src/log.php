741c7e64c3fde6fe9b18061756041c91cdcff005
Chris Wagner <christopher.t.wagner@gmail.com>
2015-08-06 22:23:32 -0400

    src/log.php: Rework API for this mini logging "framework" (hardly a framework) and place it under appropriate namespace.

--- src/log.php
+++ src/log.php
@@ -1,10 +1,44 @@
 <?php
 
+/**
+ * This file/module provides a very basic logging framework.
+ */
+
+namespace SpareParts\Log;
+
+/**
+ * Provide a file to which all log-messages should be written. If $path is null, then log-messages
+ * will be directed to 'stdout'.
+ * 
+ * @param $path string An absolute path to a file or null.
+ */
+function setLogFile($path) {
+  global $__SpareParts_Log_pathToLogFile, $__SpareParts_Log_logToStdout;
+  if ($path === null) {
+    $__SpareParts_Log_logToStdout = true;
+  } else {
+    $__SpareParts_Log_logToStdout = false;
+    $__SpareParts_Log_pathToLogFile = $path;
+  }
+}
+
+/**
+ * Provide a callable/function that will be used to format all log messages. The formatting
+ * function should accept two parameters, a severity/level (e.g. "warn") and the log-message
+ * "payload" (e.g. "something went wrong!").
+ *
+ * @param $msgFormatFunc callable A function name or a `Closure`.
+ */
+function setMessageFormatter($msgFormatFunc) {
+  global $__SpareParts_Log_msgFormatFunc;
+  $__SpareParts_Log_msgFormatFunc = $msgFormatFunc;
+}
+
 function logMsg($level, $message) {
-  global $__msgFormatFunc;
+  global $__SpareParts_Log_msgFormatFunc;
   $logLine = null;
-  if ($__msgFormatFunc) {
-    $logLine = call_user_func($__msgFormatFunc, $level, $message) . "\n";
+  if ($__SpareParts_Log_msgFormatFunc) {
+    $logLine = call_user_func($__SpareParts_Log_msgFormatFunc, $level, $message) . "\n";
   } else {
     $ip = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : null;
     $datetime = strftime('%Y-%m-%d %H:%M');
@@ -14,27 +48,25 @@ function logMsg($level, $message) {
 }
 
 function writeToLog($content) {
-  global $__logToStdout, $__pathToLogFile, $__logFileHandle;
-  if ($__logToStdout) {
+  global $__SpareParts_Log_logToStdout, $__SpareParts_Log_pathToLogFile,
+         $__SpareParts_Log_logFileHandle;
+  if ($__SpareParts_Log_logToStdout) {
     echo $content;
   } else {
-    if (empty($__pathToLogFile)) {
-      throw new Exception("No log file is configured");
+    if (empty($__SpareParts_Log_pathToLogFile)) {
+      throw new \Exception("No log file is configured");
     }
-    if (empty($__logFileHandle)) {
-      $__logFileHandle = fopen($__pathToLogFile, 'a');
+    if (empty($__SpareParts_Log_logFileHandle)) {
+      $__SpareParts_Log_logFileHandle = fopen($__SpareParts_Log_pathToLogFile, 'a');
     }
-    fwrite($__logFileHandle, $content);
+    fwrite($__SpareParts_Log_logFileHandle, $content);
   }
 }
 
-function configureLogging($path, $msgFormatFunc = null) {
-  global $__pathToLogFile, $__logToStdout, $__msgFormatFunc;
-  if ($path === null) {
-    $__logToStdout = true;
-  } else {
-    $__logToStdout = false;
-    $__pathToLogFile = $path;
+function closeLogFile() {
+  global $__SpareParts_Log_logFileHandle;
+  if (!empty($__SpareParts_Log_logFileHandle)) {
+    fclose($__SpareParts_Log_logFileHandle);
   }
-  $__msgFormatFunc = $msgFormatFunc;
+  $__SpareParts_Log_logFileHandle = null;
 }
